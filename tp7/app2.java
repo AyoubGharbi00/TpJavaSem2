95deb2255007269f14ed5f253dc59197fa7f9cf4
Ayoub Gharbi <gharbiayoub120@gmail.com>
2025-02-20 22:15:15 +0100

    Tp 7 Completé

---
+++ tp7/app2.java
@@ -0,0 +1,50 @@
+
+
+//modification
+import java.util.Scanner;
+
+public class app2 {
+    public static void main(String[]args){
+        Scanner sc =new Scanner(System.in);
+        int nl,nc=0,i,j,som=0,  produit=1;
+        int[][] M=new int [20][30];
+        System.out.println("donner le nombre de ligne");
+        nl=sc.nextInt();
+        while (nl>30) {
+            System.out.println("donner le nombre de ligne");
+        nl=sc.nextInt();
+            
+        }
+        System.out.println("donner le nombre de colonne");
+        nl=sc.nextInt();
+        while (nc>30) {
+            System.out.println("donner le nombre de colonne");
+        nl=sc.nextInt();
+    }
+    System.out.println("remplire le matrice");
+    for ( i = 0; i < nc; i++) {
+        for ( j = 0; j < nl; j++) {
+                System.out.println("M["+i+","+j+"]=");
+            M[i][j]=sc.nextInt();
+            
+            } 
+
+            
+        }
+        for ( i = 0; i < nc; i++) {
+            for ( j = 0; j < nl; j++) {
+                 som=som+M[i][j];
+                 produit=produit*M[i][j];
+                
+                } 
+    
+                
+            }
+            System.out.println("la somme de tableau"+som);
+            System.out.println("la somme de tableau"+produit);
+
+        
+    }
+
+}
+
