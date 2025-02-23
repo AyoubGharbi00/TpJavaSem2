fb9b43cd233b3592645cb2ee9dfc3a90f6b9ddd3
Ayoub Gharbi <gharbiayoub120@gmail.com>
2025-02-23 21:38:48 +0000

    app3 complet

--- tp7/app3.java
+++ tp7/app3.java
@@ -1,10 +1,10 @@
-// mm
- import java.util.Scanner;
+
+import java.util.Scanner;
 
 public class app3 {
     public static void main(String[]args){
         Scanner sc =new Scanner(System.in);
-        int nl,nc=0,i,j,som=0,  produit=1;
+        int nl,nc=0,i=0,j=0,som=0;
         int[][] M=new int [20][30];
         System.out.println("donner le nombre de ligne");
         nl=sc.nextInt();
@@ -14,32 +14,36 @@ public class app3 {
             
         }
         System.out.println("donner le nombre de colonne");
-        nl=sc.nextInt();
+        nc=sc.nextInt();
         while (nc>30) {
             System.out.println("donner le nombre de colonne");
-        nl=sc.nextInt();
+        nc=sc.nextInt();
     }
-    System.out.println("remplire le matrice");
-    for ( i = 0; i < nc; i++) {
-        for ( j = 0; j < nl; j++) {
-                System.out.println("M["+i+","+j+"]=");
-            M[i][j]=sc.nextInt();
-            
-            } 
+        System.out.println("remplire le matrice");
 
-            
+        for ( i = 0; i < nl; i++) {
+            for (j = 0; j < nc; j++) {
+                System.out.println("M["+i+","+j+"]=");
+                M[i][j]=sc.nextInt();
+            }
         }
-        for ( i = 0; i < nc; i++) {
-            for ( j = 0; j < nl; j++) {
-                 som=som+M[i][j];
-                 produit=produit*M[i][j];
+
+        for ( i = 0; i < nl; i++) {
+            for ( j = 0; j < nc; j++) {
+            
+                 System.out.print(M[i][j]+" ");
                 
-                } 
+                }System.out.println();
     
                 
             }
-            System.out.println("la somme de tableau"+som);
-            System.out.println("la somme de tableau"+produit);
+            for ( i = 0; i < nl; i++) {
+                som=0;
+                for ( j = 0; j < nc; j++) {
+                    som=som+M[i][j];
+                    
+                    }System.out.println("ligne"+(i+1)+":"+ som);
+                }      
 
         
     }
